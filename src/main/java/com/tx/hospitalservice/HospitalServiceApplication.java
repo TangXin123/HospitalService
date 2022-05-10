0bc7c5d90e811cc762919a25d9df371153762fd5
tx <278644502@qq.com>
2022-05-10 12:41:24 +0800

    1.1

--- src/main/java/com/tx/hospitalservice/HospitalServiceApplication.java
+++ src/main/java/com/tx/hospitalservice/HospitalServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HospitalServiceApplication {
 
     public static void main(String[] args) {
+        //hh
         SpringApplication.run(HospitalServiceApplication.class, args);
     }
 
